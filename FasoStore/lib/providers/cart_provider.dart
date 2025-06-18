5857404cba60741b6b02c598b30ca2e782f516b2
Philiux <philiusnostrador@gmail.com>
2025-06-18 17:01:19 +0000

    Add FasoStore/lib/providers/cart_provider.dart 1750266075307

---
+++ FasoStore/lib/providers/cart_provider.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import '../models/cart_item.dart';
+import '../models/product.dart';
+
+class CartProvider with ChangeNotifier {
+  final Map<String, CartItem> _items = {};
+
+  Map<String, CartItem> get items => _items;
+
+  int get itemCount => _items.length;
+
+  double get totalAmount {
+    double total = 0.0;
+    _items.forEach((key, cartItem) {
+      total += cartItem.product.price * cartItem.quantity;
+    });
+    return total;
+  }
+
+  void addItem(Product product) {
+    if (_items.containsKey(product.id)) {
+      _items.update(
+        product.id,
+        (existingCartItem) {
+          existingCartItem.quantity += 1;
+          return existingCartItem;
+        },
+      );
+    } else {
+      _items.putIfAbsent(
+        product.id,
+        () => CartItem(product: product),
+      );
+    }
+    notifyListeners();
+  }
+
+  void removeItem(String productId) {
+    _items.remove(productId);
+    notifyListeners();
+  }
+
+  void clear() {
+    _items.clear();
+    notifyListeners();
+  }
+}
